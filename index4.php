7273e27f221dbe9ccafa399da320b2086838a7d7
Billy Farroll <watanight@hotmail.co.uk>
2016-01-25 11:01:25 +0000

    Create index4.php

---
+++ index4.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Your own do-while</title>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+    <?php
+        $Billy = true;
+        
+        while($Billy > false)
+        
+        do {
+            
+            echo "The loop is called Billy";
+            
+        }
+        
+        while($Billy)
+        
+        echo"The loop dosen't work";
+        
+    ?>
+    </body>
+</html>
