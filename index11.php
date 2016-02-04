b90799233f6f6acbe9a774cee00c426f4da5d81d
Billy Farroll <watanight@hotmail.co.uk>
2016-02-04 11:12:09 +0000

    Create index11.php
    Using the scope with the double semi colons (::) to print something in this case the "A kingly proclamation".

---
+++ index11.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php
+        class King {
+          
+          public static function proclaim() {
+            echo "A kingly proclamation!";
+          }
+        }
+        
+        King::proclaim();
+                
+      ?>
+    </p>
+  </body>
+</html>
